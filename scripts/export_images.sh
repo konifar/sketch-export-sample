c335c6a9de955c4c8582dea99efb69369ac8c927
konifar <yahpeycoy0403@gmail.com>
2017-09-11 05:10:54 +0900

    :bug: Fixed export images shell script

--- scripts/export_images.sh
+++ scripts/export_images.sh
@@ -3,14 +3,16 @@
 destDir="$1"
 shift
 
+sketchtool=./sketchtool/bin/sketchtool
+
 exportImages() {
     local srcFile="$1"
-    local imagesDestDir = "$destDir/images"
-    mkdir -p "$imagedestDir"
-    sketchtool export slices "$srcFile" --output="$imagedestDir"
+    local imagesDestDir="$destDir/images"
+     mkdir -p "$imagesDestDir"
+    ${sketchtool} export slices "$srcFile" --output="$imagesDestDir"
 
-    (cd "$imagedestDir"; zip ../images.zip *)
-    rm -r "$imagedestDir"
+    (cd "$imagesDestDir"; zip ../images.zip *)
+    rm -r "$imagesDestDir"
 }
 
 exportImages images.sketch

2f09d68c8adbb2a4f508cee402d4efa87f29540b
konifar <yahpeycoy0403@gmail.com>
2017-09-11 20:36:20 +0900

    Modified script

--- scripts/export_images.sh
+++ scripts/export_images.sh
@@ -1,17 +1,12 @@
 #!/bin/bash
 
 destDir="$1"
-shift
 
 sketchtool=./sketchtool/bin/sketchtool
 
 exportImages() {
     local srcFile="$1"
-    local imagesDestDir="$destDir/images"
-     mkdir -p "$imagesDestDir"
-    ${sketchtool} export slices "$srcFile" --output="$imagesDestDir"
-
-    (cd "$imagesDestDir"; zip ../images.zip *)
+    ${sketchtool} export slices "$srcFile" --output="$destDir"
 }
 
 exportImages images.sketch
